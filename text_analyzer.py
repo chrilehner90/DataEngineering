c80a54d2fed20e10036f7900f8a185c8ebfce5f3
Christian Lehner <lehner.chri@gmail.com>
2016-01-07 15:27:14 +0100

    refactored and added distinct terms

--- text_analyzer.py
+++ text_analyzer.py
@@ -8,15 +8,21 @@ nltk.download("punkt")
 class TextAnalyzer:
     def __init__(self):
         self._Helper = Helper()
-        self._document_count = 0
+        self._books = []
         self._all_tokens_counts = []
+        self._distinct_tokens_counts = []
 
     def run(self):
         for f in os.listdir(self._Helper.book_directory):
             if f.endswith(".txt"):
                 book = self._Helper.read_file(f)
-                self._document_count += 1
-                self._all_tokens_counts.append(len(self.tokenize(book)))
+
+                self._books.append(book)
+                tokens = self.tokenize(book)
+
+                self._all_tokens_counts.append(len(tokens))
+
+                self._distinct_tokens_counts.append(len(self.get_distinct_tokens(tokens)))
 
     def tokenize(self, book):
         book_lower_case = book.lower()
@@ -25,15 +31,22 @@ class TextAnalyzer:
 
         return tokens_filtered
 
+    def get_distinct_tokens(self, tokens):
+        return set(tokens)
+
     def get_document_count(self):
-        return self._document_count
+        return len(self._books)
 
     def count_all_tokens_of_all_books(self):
         return sum(self._all_tokens_counts)
+
+    def count_distinct_tokens_of_all_books(self):
+        return sum(self._distinct_tokens_counts)
 
 if __name__ == "__main__":
     ta = TextAnalyzer()
     ta.run()
 
     print "# OF DOCUMENTS:", ta.get_document_count()
-    print "ALL TOKENS:", ta.count_all_tokens_of_all_books()
+    print "# OF ALL TOKENS:", ta.count_all_tokens_of_all_books()
+    print "# OF DISTINCT TOKENS:", ta.count_distinct_tokens_of_all_books()
