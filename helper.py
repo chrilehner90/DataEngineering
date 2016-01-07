1162cb5b92c8b4a25b4303032f4f36f7f67038d2
Christian Lehner <lehner.chri@gmail.com>
2016-01-07 12:19:39 +0100

    added read file function

--- helper.py
+++ helper.py
@@ -1,6 +1,12 @@
+import os
+
 class Helper:
     def __init__(self):
         self.book_directory = "downloads/"
+
+    def read_file(self, filename):
+        with open(self.book_directory + filename, "r") as f:
+            return f.read()
 
     def write_file(self, book, filename):
         with open(self.book_directory + filename, "w") as f:
