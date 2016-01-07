1359fe3e2c4f7b43299cde6a9802a607f8084c3d
Christian Lehner <lehner.chri@gmail.com>
2016-01-07 12:49:24 +0100

    refactored and added new methods

--- text_analyzer.py
+++ text_analyzer.py
@@ -5,13 +5,31 @@ from helper import Helper
 
 class TextAnalyzer:
     def __init__(self):
-        self.Helper = Helper()
+        self._Helper = Helper()
+        self._document_count = 0
+        self._all_tokens_counts = []
 
     def run(self):
-        for f in os.listdir(self.Helper.book_directory):
+        for f in os.listdir(self._Helper.book_directory):
             if f.endswith(".txt"):
-                book = self.Helper.read_file(f)
+                book = self._Helper.read_file(f)
+                self._document_count += 1
+                self._all_tokens_counts.append(len(self.tokenize(book)))
+
+    def tokenize(self, book):
+        book_lower_case = book.lower()
+        tokens = book_lower_case.split()
+        return tokens
+
+    def get_document_count(self):
+        return self._document_count
+
+    def count_all_tokens_of_all_books(self):
+        return sum(self._all_tokens_counts)
 
 if __name__ == "__main__":
     ta = TextAnalyzer()
     ta.run()
+
+    print "# OF DOCUMENTS:", ta.get_document_count()
+    print "ALL TOKENS:", ta.count_all_tokens_of_all_books()
