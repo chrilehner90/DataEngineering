c319a6dd7b39de8b1dbb00e4096a413a02b8bab3
Christian Lehner <lehner.chri@gmail.com>
2016-01-09 12:38:01 +0100

    added path for filtered books (numerics and special characters are filtered) in write function

--- helper.py
+++ helper.py
@@ -2,25 +2,30 @@ import os
 
 class Helper:
     def __init__(self):
-        self.book_directory = "downloads/"
+        self.directory_original_books = "downloads/"
+        self.directory_filtered_book = "filtered/"
         self.stemming_directory = "stemming/"
         self.lemmatization_directory = "lemmatization/"
 
     def read_file(self, filename):
-        with open(self.book_directory + filename, "r") as f:
+        with open(self.directory_original_books + filename, "r") as f:
             return unicode(f.read(), errors="ignore")
 
     def write_file(self, book, filename, dest=""):
-        directory = self.book_directory
+        directory = self.directory_original_books
 
-        if not os.path.exists(self.book_directory):
-            os.makedirs(self.book_directory)
+        if not os.path.exists(self.directory_original_books):
+            os.makedirs(self.directory_original_books)
+        if not os.path.exists(self.directory_filtered_book):
+            os.makedirs(self.directory_filtered_book)
         if not os.path.exists(self.stemming_directory):
             os.makedirs(self.stemming_directory)
         if not os.path.exists(self.lemmatization_directory):
             os.makedirs(self.lemmatization_directory)
 
-        if dest == "s":
+        if dest == "f":
+            directory = self.directory_filtered_book
+        elif dest == "s":
             directory = self.stemming_directory
         elif dest == "l":
             directory = self.lemmatization_directory
@@ -29,4 +34,4 @@ class Helper:
             print "Writing " + filename
             f.write(book)
 
-        print "Book successfully written to disk."
+        print "Book successfully written to", directory + filename
