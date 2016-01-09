69b09b384b66e0e1ffa2a34ad69be2c7f39d5786
Christian Lehner <lehner.chri@gmail.com>
2016-01-09 17:13:52 +0100

    added function for loading processed files and word frequencies

--- text_analyzer.py
+++ text_analyzer.py
@@ -13,7 +13,7 @@ class TextAnalyzer:
         self.Stemmer = SnowballStemmer("english")
         self.Lemmatizer = WordNetLemmatizer()
 
-        self.books = []
+        self.filtered_books = []
         self.stemmed_books = []
         self.lemmatized_books = []
 
@@ -38,33 +38,17 @@ class TextAnalyzer:
 
                 self.Helper.write_file(book, filename, "f")
 
-                self.books.append(book)
-
-                self.all_tokens_counts.append(len(tokens))
-
-                self.distinct_tokens_counts.append(len(self.get_distinct_tokens(tokens)))
-
                 if perform_stemming:
                     stemmed_book = self.perform_stemming(book, filename)
                     self.stemmed_books.append(stemmed_book)
 
                     self.Helper.write_file(stemmed_book, filename, "s")
 
-                    stemmed_tokens = self.tokenize(stemmed_book)
-                    self.distinct_tokens_counts_with_stemming.append(
-                            len(self.get_distinct_tokens(stemmed_tokens))
-                    )
-
                 if perform_lemmatization:
                     lemmatized_book = self.perform_lemmatization(book, filename)
                     self.lemmatized_books.append(lemmatized_book)
 
                     self.Helper.write_file(lemmatized_book, filename, "l")
-
-                    lemmatized_tokens = self.tokenize(lemmatized_book)
-                    self.distinct_tokens_counts_with_lemmatization.append(
-                            len(self.get_distinct_tokens(lemmatized_tokens))
-                    )
 
     # tokenization currently without stopword removal
     def tokenize(self, string):
@@ -97,7 +81,7 @@ class TextAnalyzer:
         return set(tokens)
 
     def get_document_count(self):
-        return len(self.books)
+        return len(self.filtered_books)
 
     def count_all_tokens_of_all_books(self):
         return sum(self.all_tokens_counts)
@@ -111,16 +95,52 @@ class TextAnalyzer:
     def count_distinct_tokens_of_all_books_with_lemmatization(self):
         return sum(self.distinct_tokens_counts_with_lemmatization)
 
-    def calculate_zipf_distribution(self):
-        return "TODO: Calculate Zipf distribution here"
+    def word_frequencies(self, identifier):
+        combined_books = ""
+        if identifier == "f":
+            for book in self.filtered_books:
+                combined_books += book
+
+        fdist = nltk.FreqDist(combined_books.split())
+
+        # sort distributions by value descending
+        sorted_fdist = sorted(fdist.items(), key=lambda x: -x[1])
+        return sorted_fdist
+
+    def load_processed_files(self):
+        print "Loading preprocessed files"
+        for filename in os.listdir(self.Helper.directory_filtered_book):
+            # filtered books
+            filtered_book = self.Helper.read_file(filename, "f")
+            self.filtered_books.append(filtered_book)
+
+            tokens = filtered_book.split()
+            self.all_tokens_counts.append(len(tokens))
+            self.distinct_tokens_counts.append(len(self.get_distinct_tokens(tokens)))
+
+            # stemmed books
+            stemmed_book = self.Helper.read_file(filename, "s")
+            stemmed_tokens = self.tokenize(stemmed_book)
+            self.distinct_tokens_counts_with_stemming.append(
+                    len(self.get_distinct_tokens(stemmed_tokens))
+            )
+
+            # lemmatized books
+            lemmatized_book = self.Helper.read_file(filename, "l")
+            lemmatized_tokens = self.tokenize(lemmatized_book)
+            self.distinct_tokens_counts_with_lemmatization.append(
+                    len(self.get_distinct_tokens(lemmatized_tokens))
+            )
 
 if __name__ == "__main__":
     ta = TextAnalyzer()
-    ta.process_text(perform_stemming=False, perform_lemmatization=False)
+    #ta.process_text(1, perform_stemming=False, perform_lemmatization=False)
+
+    ta.load_processed_files()
 
     print "# OF DOCUMENTS:", ta.get_document_count()
     print "# OF ALL TOKENS:", ta.count_all_tokens_of_all_books()
     print "# OF DISTINCT TOKENS:", ta.count_distinct_tokens_of_all_books()
     print "# OF DISTINCT TOKENS AFTER STEMMING:", ta.count_distinct_tokens_of_all_books_with_stemming()
     print "# OF DISTINCT TOKENS AFTER LEMMATIZATION:", ta.count_distinct_tokens_of_all_books_with_lemmatization()
-    print "Zipf Distribution:", ta.calculate_zipf_distribution()
+    print "Zipf Distribution:", ta.calculate_zipf_distribution("f")
